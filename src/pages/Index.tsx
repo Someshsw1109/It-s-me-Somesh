e71170f45e930c0b640706cfadca3e26856fb204
Somesh Raj <someshraj78569@gmail.com>
2025-05-02 01:30:31 +0530

    All issues fixed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/Footer';
 
 const Index = () => {
   useEffect(() => {
-    document.title = "Your Name | Portfolio";
+    document.title = "Somesh Raj | Portfolio";
   }, []);
 
   return (
